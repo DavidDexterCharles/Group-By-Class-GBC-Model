1b64767ca20831e49cc1e3509e6b3b204c781495
David <gogs@fake.local>
2024-02-01 13:58:30 -0400

    working in train function

--- gbc.py
+++ gbc.py
@@ -7,25 +7,51 @@
 from typing import List
 from document_service import DocumentService,Document
 
-class GroupByClassTrain:
+class GroupByClassModel:
     '''
     Implementation of  the Group By Class Machine Learning Algorithms
     Features include model training,text classification,incremental learning, key word extraction
     '''
     def __init__(self,name):
-        self.document_service:DocumentService=DocumentService()
+        self.model_trained=False
+        self.ds:DocumentService=DocumentService()
         self.name = name
-        self.labels = []
+        self.categories = []
         self.number_of_documents=0
+
+    def _categorylist(self,documents):
+        all_categories = []
+        for document in documents:
+            all_categories.extend(document.categories)
+        unique_categories = list(set(all_categories))
+        self.categories=unique_categories
 
     def train(self,json_data):
         '''
         trains model, stores number of documents
         '''
-        documents:List[Document]=self.document_service.json_to_documents(json_data)
-        # print(d[0].content)
+        if self.model_trained:
+            print("model already trained")
+            return
+        documents:List[Document]=self.ds.json_to_documents(json_data)
         self.number_of_documents=len(documents)
-        print(self.number_of_documents)
+        self._categorylist(documents)
+
+    def get_number_of_documents(self):
+        '''
+        returns number of docs used to train model
+        '''
+        return self.number_of_documents
+
+    def get_categories(self,doprint=False):
+        '''
+        returns the list of categories/labels
+        the model contains
+        '''
+        c=self.categories
+        if doprint:
+            print(c)
+        return c
 
     def print_model_name(self):
         '''
