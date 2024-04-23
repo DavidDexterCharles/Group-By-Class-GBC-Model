1ed324d8dd206fc29175b007db047f2acbb428e6
David <gogs@fake.local>
2024-04-23 08:30:52 -0400

    mongodb setup and routes

--- app/api/endpoints/articles.py
+++ app/api/endpoints/articles.py
@@ -1,13 +1,14 @@
 # pylint: disable=C0115:missing-class-docstring,C0114:missing-module-docstring
 # pylint: disable=C0301:line-too-long
 import json
-import redis
+# import redis
 from fastapi import APIRouter
 from app.api.pydanticmodels import Article
 from app.services.gbc import GroupByClassModel
 router = APIRouter()
 
-
+# https://youtu.be/VQnmcBnguPY?t=245
+# accessing mongodb cluster
 @router.get("/articles")
 def main():
     '''
@@ -21,7 +22,7 @@ async def articles(request_data: list[Article]):#, db = Depends(get_db)):
     '''
     add articles
     '''
-    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    # redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
     result="test"
     # jsondata=request_data#json.dump(request_data)
@@ -31,3 +32,15 @@ async def articles(request_data: list[Article]):#, db = Depends(get_db)):
     # model.get_categories(True)
     result=model.classify("Congratulations! You've been selected as the winner of our exclusive contest. Claim your prize now!")
     return  result
+
+@router.post("/train")
+async def train(request_data: list[Article],increment_learning:bool=True):#, db = Depends(get_db)):
+    '''
+    add articles
+    '''
+    # jsondata=request_data#json.dump(request_data)
+    model=GroupByClassModel(increment_learning=increment_learning)
+    model.train(request_data)
+    # # model3.train(example_json_data2)
+    # model.get_categories(True)
+    return  model.model_class_vectors
