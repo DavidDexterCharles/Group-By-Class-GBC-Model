477840f6ab064d5df4c38b659c25b9e301cffec4
David Charles <david.dexter.charles@gmail.com>
2024-04-24 07:27:06 -0400

    cnn type up https://youtu.be/t0EzVCvQjGE?t=1072

--- app/api/endpoints/cnn.py
+++ app/api/endpoints/cnn.py
@@ -45,4 +45,23 @@ def cnn():
     
     plt.show()
 
+    training_images=training_images[:20000]
+    training_labels=training_labels[:20000]
+    testing_labels=testing_labels[:4000]
+    testing_labels=testing_labels[:4000]
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
+    model.add(layers.MaxPool2D((2,2)))
+    model.add(layers.Conv2D(64,(3,3),activation='relu'))
+    model.add(layers.MaxPool2D((2,2)))
+    model.add(layers.Conv2D(64,(3,3),activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64,activation='relu'))
+    model.add(layers.Dense(10,activation='softmax'))# so that there is one classification output that adds up to 1
+
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+    model.fit(training_images,training_labels,epochs=10,validation_data=(testing_images,testing_labels))
+
     return "CNN VIEW"
