af6d4eb2e7150efa80cdf802062aa8f31a9a0408
David Charles <david.dexter.charles@gmail.com>
2024-04-24 07:08:01 -0400

    cnn adding

--- app/api/endpoints/cnn.py
+++ app/api/endpoints/cnn.py
@@ -35,4 +35,14 @@ def cnn():
     (training_images,training_labels),(testing_images,testing_labels)=datasets.cifar10.load_data()
     training_images,testing_images=training_images/255,testing_images/255
     class_names=['Plane','Car','Bird','Cat','Deer','Dog','Frog','Horse','Ship','Truck']
+
+    for i in range(16):
+        plt.subplot(4,4,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(training_images[i],cmap=plt.cm.binary)
+        plt.xlabel(class_names[training_labels[i][0]])
+    
+    plt.show()
+
     return "CNN VIEW"
