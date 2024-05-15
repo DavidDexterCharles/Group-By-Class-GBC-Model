3880e794762b53da2d0a9a1d7953b11986ebdb0d
David <gogs@fake.local>
2024-05-15 15:32:31 -0400

    progress so far

--- labels/mergecontent.py
+++ labels/mergecontent.py
@@ -7,10 +7,10 @@ def merge_text_files(output_file, *input_files):
 
 # Example usage:
 
-categories=['health_emergnecies','mentalhealth']
+categories=['economic_impact','health_emergnecies','mentalhealth','pandemics','personal_experience','preventative_measures']
 # C:\Users\30013717\Documents\Personal\GroupByClassModel\labels\health_emergnecies\health_emergnecies_gemini.txt
 for cindex,i in  enumerate(categories):
     input_files = [f'labels/{i}/{i}_gpt.txt', f'labels/{i}/{i}_gemini.txt', f'labels/{i}/{i}_mistral.txt']
-    output_file = f'merged_{i}.txt'
+    output_file = f'labels/merged_{i}.txt'
     merge_text_files(output_file, *input_files)
     print(f"{i} Files merged successfully!")
